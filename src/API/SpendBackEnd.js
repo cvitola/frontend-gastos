a31933caefde98732271d8bb44849e2276e1271c
Cristian <isrunaway@gmail.com>
2022-07-28 09:33:55 -0300

    Correccion tryCatch

---
+++ src/API/SpendBackEnd.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const baseURL = 'https://myspends-api.herokuapp.com/api';
+
+export const createNewSpend = async(date,category,amount) => {
+        const response = await axios.post(`${baseURL}/spends`, {
+                date: date,
+                category: category,
+                amount: amount
+        }); 
+        return response
+
+}  
+
--- src/components/Gasto/AltaGasto/AltaGasto.js
+++ src/components/Gasto/AltaGasto/AltaGasto.js
@@ -1,21 +1,30 @@
 import React, {useState} from 'react'
 import { Button, ContainerFirst, H1, Input } from '../../BasicStyles/BasicStyles'
 import { BoxGasto } from '../GastoStyles'
-//import { createNewSpend } from "../../../API/SpendBackEnd";
+import  { createNewSpend }  from "../../../API/SpendBackEnd";
 
 const AltaGasto = () => {
     const [date, setDate] = useState("");
     const [category, setCategory] = useState("");
     const [amount, setAmount] = useState("");
+    const [message, setMessage] = useState("");
 
     const handleOnChangeDate = (e) => {setDate(e.target.value)}
     const handleOnChangeCategory = (e) => {setCategory(e.target.value)}
     const handleOnChangeAmount = (e) => {setAmount(e.target.value)}
 
     const handleNewSpend = async() => {
-       /* //primero sin validar para recuperar respuiesta de BackEnd.
+      //primero sin validar para recuperar respuiesta de BackEnd.
+      try{
         const response = await createNewSpend(date,category,amount);
-        console.log(response);*/
+        if (!response.ok) {
+          throw new Error("No se pudo procesar la respuesta");
+        }  
+        return response;    
+      } catch(error){
+          console.log(error.response)
+          setMessage(error.response.data)
+      }
 
     }
 
@@ -33,6 +42,7 @@ const AltaGasto = () => {
             <Input type="text" placeholder='Monto...' value={amount} onChange={handleOnChangeAmount}/>
             <Button onClick={handleNewSpend}>Enviar</Button>
             <Button onClick={handleClearFields}warning={true}>Cancelar</Button>
+            <p>Hola: {message}</p>
         </BoxGasto>
     </ContainerFirst>
   )
